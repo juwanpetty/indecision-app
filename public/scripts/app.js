3b87a3f346e1c86c7fb04e4ce18c59522dc50c0f
juwanpetty <jchpetty@gmail.com>
2018-02-23 16:28:56 -0500

    Finished barebones app functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,6 +26,12 @@ var onRemoveAll = function onRemoveAll() {
   renderApp();
 };
 
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var renderApp = function renderApp() {
@@ -48,9 +54,9 @@ var renderApp = function renderApp() {
       app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
-      'p',
-      null,
-      app.options.length
+      'button',
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What should I do?'
     ),
     React.createElement(
       'button',
@@ -60,16 +66,13 @@ var renderApp = function renderApp() {
     React.createElement(
       'ol',
       null,
-      React.createElement(
-        'li',
-        null,
-        'Item one'
-      ),
-      React.createElement(
-        'li',
-        null,
-        'Item two'
-      )
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          option
+        );
+      })
     ),
     React.createElement(
       'form',
--- src/app.js
+++ src/app.js
@@ -24,6 +24,12 @@ const onRemoveAll = () => {
   renderApp();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum]
+  alert(option);
+};
+
 const appRoot = document.getElementById('app');
 
 const renderApp = () => {
@@ -33,13 +39,13 @@ const renderApp = () => {
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
       
-      <p>{app.options.length}</p>
-
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
       <button onClick={onRemoveAll}>Remove All</button>
 
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        {
+          app.options.map(option => <li key={option}>{option}</li>)
+        }
       </ol>
 
       <form onSubmit={onFormSubmit}>
