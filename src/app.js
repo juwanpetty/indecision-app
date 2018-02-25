5b219b2ac43b5fb81c9cc0408fdb3b201321d636
juwanpetty <jchpetty@gmail.com>
2018-02-25 12:08:46 -0500

    Created nested structure with React components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -22,7 +35,18 @@ class Action extends React.Component {
 class Options extends React.Component {
   render() {
     return (
-      <p>Options Component</p>
+      <div>
+        <p>Options Component</p>
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <p>Option Component</p>
     );
   }
 }
@@ -35,13 +59,5 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
