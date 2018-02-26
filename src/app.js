497f5a841a5b3d6382df6aa733158c6281f9d153
juwanpetty <jchpetty@gmail.com>
2018-02-25 20:59:54 -0500

    Passing data into components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer.';
+    const options = ['Thing One', 'Thing Two', 'Thing Three'];
+
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle }/>
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
@@ -15,8 +19,8 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer.</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -36,8 +40,9 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        <p>Options Component</p>
-        <Option />
+        {
+          this.props.options.map(option => <Option key={option} optionText={option} />)
+        }
       </div>
     );
   }
@@ -46,7 +51,9 @@ class Options extends React.Component {
 class Option extends React.Component {
   render() {
     return (
-      <p>Option Component</p>
+      <div>
+        {this.props.optionText}
+      </div>
     );
   }
 }
