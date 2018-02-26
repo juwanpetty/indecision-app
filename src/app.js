fc06a5412244307da3a7c4874d2d080c3dbca968
juwanpetty <jchpetty@gmail.com>
2018-02-25 22:34:59 -0500

    Added JSX event attributes to components

--- src/app.js
+++ src/app.js
@@ -27,19 +27,28 @@ class Header extends React.Component {
 } 
 
 class Action extends React.Component {
+  handlePick() {
+    alert('Handle Pick');
+  }
+
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('Removed All');
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map(option => <Option key={option} optionText={option} />)
         }
@@ -59,9 +68,24 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+  
+    if (option) {
+      e.target.elements.option.value = '';
+
+      alert(option);
+    }
+  }
+
   render() {
     return (
-      <p>AddOption Component</p>
+      <form onSubmit={this.handleAddOption}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     );
   }
 }
