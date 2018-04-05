f7a04f25a1ad3a37f2949fa168572a6e5b8b1722
juwanpetty <jchpetty@gmail.com>
2018-04-04 22:08:35 -0400

    Manual Data Binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -44,40 +44,50 @@ var template = React.createElement(
 
 var count = 0;
 var addOne = function addOne() {
-    console.log('addOne');
+    count++;
+    renderCounterApp();
 };
+
 var minusOne = function minusOne() {
-    console.log('minusOne');
+    count--;
+    renderCounterApp();
 };
+
 var resetCount = function resetCount() {
-    console.log('resetCount');
+    count = 0;
+    renderCounterApp();
 };
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Count: ',
-        count
-    ),
-    React.createElement(
-        'button',
-        { onClick: addOne },
-        '+1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: resetCount },
-        'Reset'
-    ),
-    React.createElement(
-        'button',
-        { onClick: minusOne },
-        '-1'
-    )
-);
-console.log(templateTwo);
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: resetCount },
+            'Reset'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        )
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
