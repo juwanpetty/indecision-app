f90a7d1d056d7400a794c0ac7d9a0a9d9e79bddd
juwanpetty <jchpetty@gmail.com>
2018-05-18 20:41:21 -0400

    Experiment with named exports

---
+++ src/person.js
@@ -0,0 +1,9 @@
+console.log('person.js is running...');
+
+export const isAdult = age => age >= 18 ? true : false;
+export const canDrink = age => age >= 21 ? true : false;
+
+// export {
+//     isAdult,
+//     canDrink
+// };
